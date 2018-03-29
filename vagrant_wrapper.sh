10aa95f622ba12cdea6f8caf8354289e84e24b56
Michael Ashe <mickjohnashe@hotmail.com>
2018-03-29 21:26:56 +0100

    Adding users role Moved user tasks from hardening to users

--- vagrant_wrapper.sh
+++ vagrant_wrapper.sh
@@ -14,7 +14,7 @@ function vagrant_ssh() {
 }
 
 function provision() {
-  ansible-playbook -i inventory_test playbooks/role_test.yml -k -u vagrant
+  ansible-playbook -i inventory_test playbooks/main.yml -k -u vagrant
 }
 
 case $1 in
